e8b8b8ef374290b53d523b6c543f0b26f974399a
hparameswaran <harishp@hotmail.com>
2018-06-24 08:43:57 -0400

    HATEOAS implementation done - reply will have _links

--- src/main/java/com/harish/restful/controller/UserResource.java
+++ src/main/java/com/harish/restful/controller/UserResource.java
@@ -1,9 +1,18 @@
 package com.harish.restful.controller;
 
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +32,7 @@ public class UserResource {
 	@Autowired
 	private UserDaoService service;
 	
+	//HATEOAS  - Hypermedia As The Engine Of Application State
 	
 	//retrieveAllUsers
 	@GetMapping("/users")
@@ -32,15 +42,31 @@ public class UserResource {
 	
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id){
+	public Resource<User>  retrieveUser(@PathVariable int id){
 		User user = service.findOne(id);
 		if(user==null)
 			throw new UserNotFoundException("id : " + id);
-		return user;
+		
+		//"all-users", SERVER_PATH + "/users"
+		//retrieveAllUsers   -- it will add a _links section to reply to give back
+		Resource<User> resource = new Resource<User>(user);
+		
+		ControllerLinkBuilder linkSelf = 
+				linkTo(methodOn(this.getClass()).retrieveUser(id));
+		ControllerLinkBuilder linkTo = 
+				linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		
+		resource.add(linkTo.withRel("all-users"));
+		resource.add(linkSelf.withRel("specific-user"));
+				
+		//HATEOAS
+		return resource;
 	}
 	
+	//@Valid will check that the incoming User object fields adhere to the validation rules defined in the entity 
+	// and otherwise return 400 Bad Request
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user){
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
 		User savedUser = service.save(user);
 		
 		// /user/{id}  savedUser.getId()
--- src/main/java/com/harish/restful/entity/User.java
+++ src/main/java/com/harish/restful/entity/User.java
@@ -2,12 +2,18 @@ package com.harish.restful.entity;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
+	
 	private Integer id;
+	@Size(min=2)
 	private String name;
+	@Past
 	private Date birthDate;
 	
-	public User() {
+	protected User() {
 		
 	}
 	public User(Integer id, String name, Date birthDate) {
